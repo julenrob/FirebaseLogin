15bf8b9ddec7bd3edb4a441396797300bda8d138
julenrob <julenmartinez4@gmail.com>
2022-01-18 14:48:54 +0100

    Initial commit

---
+++ app/src/main/java/com/julenrob/firebaselogin/AuthActivity.kt
@@ -0,0 +1,145 @@
+package com.julenrob.firebaselogin
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.julenrob.firebaselogin.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+    lateinit var binding : ActivityAuthBinding
+    private val GOOGLE_SIGN_IN = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val analytics:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa.")
+        analytics.logEvent("InitScreen", bundle)
+
+        setup()
+        session()
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        binding.authLayout.visibility = View.VISIBLE
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email : String? = prefs.getString("email", null)
+        val provider : String? = prefs.getString("provider", null)
+
+        if (email != null && provider != null){
+            binding.authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
+
+    }
+
+    private fun setup() {
+
+        title = "Autenticación"
+        binding.btnRegister.setOnClickListener {
+            if (binding.etEmail.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().
+                createUserWithEmailAndPassword(
+                    binding.etEmail.text.toString(),
+                    binding.etPassword.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+        binding.btnAcceder.setOnClickListener {
+            if (binding.etEmail.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().
+                signInWithEmailAndPassword(
+                    binding.etEmail.text.toString(),
+                    binding.etPassword.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+        binding.btnGoogle.setOnClickListener {
+
+            // Conf
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog : AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email : String, provider : ProviderType){
+        val homeIntent = Intent(this, HomeActivity::class.java).apply{
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null){
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            showHome(account.email ?: "", ProviderType.GOOGLE)
+                        } else {
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e:ApiException){
+                showAlert()
+            }
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/julenrob/firebaselogin/HomeActivity.kt
@@ -0,0 +1,77 @@
+package com.julenrob.firebaselogin
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.julenrob.firebaselogin.databinding.ActivityAuthBinding
+import com.julenrob.firebaselogin.databinding.ActivityHomeBinding
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE
+}
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding : ActivityHomeBinding
+    private val db = FirebaseFirestore.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // SETUP
+        val bundle : Bundle? = intent.extras
+        val email : String? = bundle?.getString("email")
+        val provider : String? = bundle?.getString("provider")
+        // En el caso de que no existan los campos email y provider se
+        // enviará "", una string vacia.
+        setup(email ?: "", provider ?: "")
+
+        // SHARED PREFERENCES
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+
+    }
+
+    private fun setup(email : String, provider : String) {
+        title = "Inicio"
+        binding.tvEmail.text = email
+        binding.tvProvider.text = provider
+
+        binding.btnLogout.setOnClickListener{
+
+            // BORRAR DATOS DE SHARED PREFERENCES
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
+        binding.btnDbGuardar.setOnClickListener{
+            db.collection("users").document(email).set(
+                hashMapOf("provider" to provider,
+                "address" to binding.tvDireccion.text.toString(),
+                "phone" to binding.tvTelefono.text.toString())
+            )
+        }
+
+        binding.btnDbRecuperar.setOnClickListener{
+            db.collection("users").document(email).get().addOnSuccessListener{
+                binding.tvDireccion.setText(it.get("address") as String?)
+                binding.tvTelefono.setText(it.get("phone") as String?)
+            }
+        }
+
+        binding.btnDbEliminar.setOnClickListener{
+            db.collection("users").document(email).delete()
+        }
+
+    }
+}
